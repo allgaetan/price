271c16701dcbbf32004b75423c9d559465678cb9
allgaetan <gaet.allaire@gmail.com>
2024-02-06 19:08:02 +0100

    First tests

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="red">{{ msg }}</h1>
     <h3>Bienvenue sur la plateforme Mines'Ramax</h3>
   </div>
 </template>
